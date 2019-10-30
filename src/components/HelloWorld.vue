7d32d84877b293cb88d33e754063726708f35695
guohaojie <guohaojie@bupt.edu.cn>
2019-10-30 14:19:45 +0800

    feat(): 增加切换按钮

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="content">
         <div class="query">
+            <div class="query-select">
+                <button
+                    class="query-select-button"
+                    @click="changeCurrent('add')"
+                >+</button>
+                <div class="query-select-message">当前读取的是 {{ current }}.json</div>
+                <button
+                    class="query-select-button"
+                    @click="changeCurrent('del')"
+                >-</button>
+            </div>
             <div class="item">
                 <video
                     v-if="isVideo"
@@ -19,6 +30,7 @@
         </div>
         <div class="detail">
             <div
+                ref="left"
                 :style="{ 'height': windowHeight + 'px' }"
                 class="detail-fp"
             >
@@ -79,7 +91,9 @@ export default {
             // 获取不同设备屏幕的浏览器高度从而更好地适配滚动效果
             windowHeight: 0,
             // 判断是video还是image
-            isVideo: 0
+            isVideo: 0,
+            // 当前读取json文件
+            current: 1
         };
     },
     mounted() {
@@ -90,7 +104,7 @@ export default {
     },
     methods: {
         getImages() {
-            this.axios.get('/static/test.json').then((res) => {
+            this.axios.get(`/static/${this.current}.json`).then((res) => {
                 this.query = res.data.query;
                 let end = this.query.imageUrl.length - 3;
                 if (end > 0 && this.query.imageUrl.lastIndexOf('mp4') === end) {
@@ -98,8 +112,33 @@ export default {
                 }
                 this.fp = res.data.fp;
                 this.fn = res.data.fn;
+            }).catch(err => {
+                console.log('err', err);
+                this.current--;
+                this.showToast();
             });
+        },
+        changeCurrent(e) {
+            if (e === 'del') {
+                this.current === 1 ? this.showToast() : this.current--; this.getImages();
+            }
+            else {
+                this.current++;
+                this.getImages();
+            }
+        },
+        showToast() {
+            alert('没有更多的数据了～');
         }
+        // 监听元素滚动事件获取高度并调用对应函数
+        // scrollLeft(e) {
+        //     console.log('e', e);
+        //     console.log('left', this.$refs.left);
+        //     console.log(this.$refs.left.scrollToTop);
+        //     // if (left === this.windowHeight) {
+        //     //     console.log('滑倒了底部');
+        //     // }
+        // }
     }
 };
 </script>
@@ -110,15 +149,28 @@ export default {
     // height: 100%;
     // overflow: hidden;
     .query {
-        text-align: center;
-        // padding: 20px 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        &-select{
+            &-button {
+                width: 80px;
+                height: 40px;
+                font-size: 20px;
+                border-radius: 10px;
+            }
+            &-message {
+                margin: 15px 0;
+                font-weight: bold;
+            }
+        }
     }
     .detail {
         display: flex;
         &-fp {
             display: flex;
             flex-wrap: wrap;
-            justify-content: space-around;
+            // justify-content: space-around;
             width: 50%;
             border: solid 1px;
             overflow-y: auto;
